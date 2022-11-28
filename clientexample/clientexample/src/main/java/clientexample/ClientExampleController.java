c653d00277324ea0e74db8c8fcf7deec62b8eead
pc <pc@DESKTOP-QS34LQT>
2022-11-28 15:52:20 +0900

    asdfasdf

--- clientexample/clientexample/src/main/java/clientexample/ClientExampleController.java
+++ clientexample/clientexample/src/main/java/clientexample/ClientExampleController.java
@@ -20,7 +20,7 @@ public class ClientExampleController {
 	UIData uiData = new UIData();
 
 	public String serviceUrl() {
-	    return "basicexample220202196.223.130.168.242.traefik.me";
+	    return "http://localhost:8012/employee";
 	}
 	
     @RequestMapping(value="/emplist", method=RequestMethod.GET)
